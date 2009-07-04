0cba6e2648b2f593886cc1ec84a87b4f69f306ab
Evgeny.Myasishchev <evgeny.myasishchev@infora.com.ua>
2009-07-04 23:51:48 +0300

    controller action parameters parsing implemented. refs 300

--- lib/remote_controller/base.rb
+++ lib/remote_controller/base.rb
@@ -1,3 +1,35 @@
 class RemoteController::Base
   
+  class RemoteControllerError < StandardError #:nodoc:
+  end
+  
+  def initialize(url)
+    @url = url
+  end
+  
+  def method_missing(symbol, *args)
+    
+    #Following stuff is required to send requests to remote controllers
+    action_name = symbol.to_s
+    method = :get
+    parameters = {}
+    
+    #Processing arguments
+    if args.length > 0
+      method = args.shift if([:get, :post].include?(args[0]))
+    end
+    if args.length > 1 || (args.length == 1 && !args[0].is_a?(Hash))
+      raise RemoteControllerError.new("Invalid arguments.")
+    elsif args.length == 1
+      parameters = args.shift
+    end
+    
+    #Now we can send the request
+    send_request(action_name, method, parameters)
+  end
+  
+  private
+    def send_request(action_name, method, parameters)
+      
+    end
 end
--- test/remote_controller_test.rb
+++ test/remote_controller_test.rb
@@ -2,16 +2,52 @@ require  File.dirname(__FILE__) + '/test_helper'
 
 class RemoteControllerTest < ActiveSupport::TestCase
   
+  SERVER_PORT = 4982
+  
   def setup
-    @context = RemoteController::Testing::HttpContext.new(3000)
+    @context = RemoteController::Testing::HttpContext.new(SERVER_PORT)
   end
   
-  def test_something
-    @context.start do |request, response|
-      puts request
-      puts response
+  def test_arguments_parsing
+    controller = RemoteController::Base.new("http://localhost:#{SERVER_PORT}/test_controller")
+    instance = self
+    
+    controller.class.send(:define_method, :send_request) do |action_name, method, parameters|
+      instance.assert_equal "no_args_get", action_name
+      instance.assert_equal :get, method
+      instance.assert_equal 0, parameters.size
     end
-    @context.wait
+    controller.no_args_get
+    
+    controller.class.send(:define_method, :send_request) do |action_name, method, parameters|
+      instance.assert_equal "no_args_post", action_name
+      instance.assert_equal :post, method
+      instance.assert_equal 0, parameters.size
+    end
+    controller.no_args_post(:post)
+
+    controller.class.send(:define_method, :send_request) do |action_name, method, parameters|
+      instance.assert_equal "post_with_args", action_name
+      instance.assert_equal :post, method
+      instance.assert_equal({:id => 10}, parameters)
+    end
+    controller.post_with_args(:post, {:id => 10})
+    
+    controller.class.send(:define_method, :send_request) do |action_name, method, parameters|
+      instance.assert_equal "get_with_args", action_name
+      instance.assert_equal :get, method
+      instance.assert_equal({:id => 10}, parameters)
+    end
+    #Default method is get
+    controller.get_with_args({:id => 10})    
+    
+    assert_raise RemoteController::Base::RemoteControllerError do
+      controller.invalid_args_action("invalid arg")
+    end
+    
+    assert_raise RemoteController::Base::RemoteControllerError do
+      controller.invalid_args_action(:get, "invalid arg")
+    end
   end
   
 end
